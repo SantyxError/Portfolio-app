d6d1d9431f76b4e0d2d76184eedacc31a9028780
Santiago Benito <bmsanti@hotmail.com>
2024-11-14 17:24:18 +0100

    Header, sidebar and card components added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'], 
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'portfolio-app';
+  public cards: Array<any> = [];
+
+  ngOnInit(): void {
+    this.cards = [
+      { title: 'Video 1', subtitle: 'Video 1' },
+      { title: 'Video 2', subtitle: 'Video 2' },
+      { title: 'Video 3', subtitle: 'Video 3' },
+    ];
+    console.log('AppComponent inicializado');
+  }
 }
